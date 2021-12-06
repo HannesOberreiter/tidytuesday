3bc37f9dcdae9181fba3f6aea82912323fb18932
HannesOberreiter <hoberreiter@gmail.com>
2021-12-06 09:47:07 +0100

    added plot

--- aoc/2021/06/index.R
+++ aoc/2021/06/index.R
@@ -20,7 +20,7 @@ for (day in 1:days) {
 length(history[[days]])
 
 # Part 2
-fish <- as.numeric(rep(0, 9))
+fish <- as.numeric(rep(0, 9)) # integer gives integer overflow :/
 names(fish) <- 0:8
 fish[names(table(data))] <- table(data)
 days <- 256
@@ -31,3 +31,54 @@ for (day in 1:days) {
     fish[7] <- fish[7] + spawn
 }
 format(sum(fish), scientific = FALSE)
+
+
+# Plotting History
+fish <- as.numeric(rep(0, 9)) # integer gives integer overflow :/
+names(fish) <- 0:8
+fish[names(table(data))] <- table(data)
+history <- list()
+days <- 256
+for (day in 1:days) {
+    spawn <- fish[1]
+    fish[1:8] <- fish[2:9]
+    fish[9] <- spawn
+    fish[7] <- fish[7] + spawn
+    history[[day]] <- fish
+}
+format(sum(fish), scientific = FALSE)
+
+library(gganimate)
+df <- bind_rows(history) %>%
+    mutate(daysum = rowSums(.)) %>%
+    rowid_to_column("day") %>%
+    pivot_longer(-c("day", "daysum")) %>%
+    mutate(
+        day_string = glue::glue("Day: {day} || Laternfish: {daysum}"),
+        day_string = forcats::fct_reorder(day_string, day)
+    ) %>%
+    ggplot(aes(x = name, y = value)) +
+    geom_col() +
+    scale_y_log10(
+        # trans = scales::pseudo_log_trans(),
+        breaks = scales::log_breaks(),
+        labels = scales::label_number_si()
+    ) +
+    labs(
+        title = "AoC - Day 6: Lanternfish",
+        subtitle = "Day: {frame_time}",
+        x = "Spawning Counter",
+        y = "Count [#]",
+        caption = "Advent of Code 2021, Hannes Oberreiter"
+    ) +
+    theme_classic() +
+    theme(
+        panel.grid.major.y = element_line(linetype = "dashed"),
+        axis.title.y = element_text(margin = margin(l = 50, r = -50)),
+        plot.caption = element_text(hjust = 1, size = 5)
+    ) +
+    transition_time(day) +
+    ease_aes("linear") +
+    enter_grow()
+
+anim_save("output/figs/aoc06.gif", animate(df, fps = 8))
